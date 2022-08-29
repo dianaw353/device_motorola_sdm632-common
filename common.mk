2f07d718d86fdf56533cc22dc75fca8ab4913f71
Botan Yuukan <65094216+Botan-Yuukan@users.noreply.github.com>
2022-08-29 12:36:25 -0600

    android.hardware.power.stats@1.0::IPowerStats/default [1/2]

--- common.mk
+++ common.mk
@@ -371,6 +371,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power.stats@1.0-service.mock \
     android.hardware.power-service-qti \
     android.hardware.power@1.2.vendor \
     vendor.qti.hardware.perf@2.0.vendor
